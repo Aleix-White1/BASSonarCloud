240141f3df4b0e5f3885ff96d2197435afb4c82f
Suport Launchpad <suportlaunchpad@tmb.cat>
2024-02-08 10:37:38 +0000

    quitar errores

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device"
-],
+], 
     /**
      * provide app-view type models (as in the first "V" in MVVC)
      * 
@@ -15,89 +15,9 @@ sap.ui.define([
 
         return {
             createDeviceModel: function () {
-                let myArray = ['a', 'b', 'c', 'd'];
                 var oModel = new JSONModel(Device);
-                if (oModel === NaN) {
-                    oModel.setDefaultBindingMode("OneWay");
-                    return oModel;
-                    delete myArray[2];
-                }
-                let SmyArray = ['a', 'b', 'c', 'd'];
-                var oModel = new JSONModel(Device);
-                if (oModel === NaN) {
-                    oModel.setDefaultBindingMode("OneWay");
-                    return oModel;
-                    delete myArray[2];
-                }
-                let SDmyArray = ['a', 'b', 'c', 'd'];
-                var oModel = new JSONModel(Device);
-                if (oModel === NaN) {
-                    oModel.setDefaultBindingMode("OneWay");
-                    return oModel;
-                    delete myArray[2];
-                }
-                let DAmyArray = ['a', 'b', 'c', 'd'];
-                var oModel = new JSONModel(Device);
-                if (oModel === NaN) {
-                    oModel.setDefaultBindingMode("OneWay");
-                    return oModel;
-                    delete myArray[2];
-                }
-                let FDmyArray = ['a', 'b', 'c', 'd'];
-                var oModel = new JSONModel(Device);
-                if (oModel === NaN) {
-                    oModel.setDefaultBindingMode("OneWay");
-                    return oModel;
-                    delete myArray[2];
-                }
-                x = [1, 2];
-                var y = x / 4;
-                console.log(y);
-
-                'foo'.match(/bar/);
-                numbers.sort(); // Noncompliant: lexicographic sort
-                console.log(numbers); // O
-
-                const wnumbers = [10, 2, 30, 1, 5];
-                numbers.sort(); // Noncompliant: lexicographic sort
-                console.log(numbers); // O
-
-                const snumbers = [10, 2, 30, 1, 5];
-                numbers.sort(); // Noncompliant: lexicographic sort
-                console.log(numbers); // O
-
-                const dnumbers = [10, 2, 30, 1, 5];
-                numbers.sort(); // Noncompliant: lexicographic sort
-                console.log(numbers); // O
-
-                const dumbers = [10, 2, 30, 1, 5];
-                numbers.sort(); // Noncompliant: lexicographic sort
-                console.log(numbers); // O
-
-                if (unexpectedCondition) {
-                    alert("Unexpected Condition");
-                }
-                console.log(checkEqual(0, false)); // Output: "Equal"
-
-                const numbers = [10, 2, 30, 1, 5];
-                
-                if (numbers.indexOf("blue") > 0){
-                    console.log("Hola")
-                }
-
-                if (value != value){processNaN(value);}
-                    
-
-                if (a == b) { // Noncompliant: using non-strict equality '=='
-                    return NaN;
-                } else {
-                    return NaN;
-                
-                }
-                
-
-            }
-
-
-        };
-    });
+                oModel.setDefaultBindingMode("OneWay");
+                return oModel;
+        }
+    };
+});
